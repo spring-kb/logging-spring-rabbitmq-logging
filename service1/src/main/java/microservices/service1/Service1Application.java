7af475cb7eab7274412d26e23b7bb596c8779642
galkzaz <gamal.m.alkzaz@gmail.com>
2024-10-02 23:40:25 +0300

    Revert "initial commit"
    This reverts commit 612cdbfd80a245702bfe9a8305c301b8f912a479.

--- service1/src/main/java/microservices/service1/Service1Application.java
+++ service1/src/main/java/microservices/service1/Service1Application.java
@@ -1,7 +1,10 @@
 package microservices.service1;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Service1Application {
